771472794e9033e6ed6fc128f03038728c9ef7e3
abtd <abaotiandi1205@outlook.com>
2021-09-09 09:35:20 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,9 @@
+import { defineUserConfig } from 'vuepress';
+import type { DefaultThemeOptions } from 'vuepress';
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-CN',
+  title: '考研小记',
+  description: '考研小记',
+  base: '/postgraduate-admission-test-log/'
+});
